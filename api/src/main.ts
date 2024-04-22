559af177d21faa598ee96aa5f5be3f8e948c2590
WindyJasmin <123154941+WindyJasmin@users.noreply.github.com>
2024-04-22 15:44:11 +0100

    adding cors settings

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,18 @@
 /* eslint-disable prettier/prettier */
-
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
-
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization', 
+    methods: 'GET, POST, PUT, DELETE', 
+  });
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
+
