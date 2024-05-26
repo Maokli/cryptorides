a5d68b05fdd2eeb3cad9d8d8d3a2e23aa3572176
chamsTmar1 <chamstmar@gmail.com>
2024-05-26 20:25:46 +0100

    added toaster -> time to test

--- client/src/helpers/axios.helpers.ts
+++ client/src/helpers/axios.helpers.ts
@@ -1,6 +1,54 @@
 import axios from 'axios';
 import { isAuthenticated, getUserToken } from './auth.helpers';
-import { notifySuccess, notifyError } from './toast.helpers';
+import { toast } from 'react-toastify';
+
+const notifySuccess = () => {
+  toast.success("Request successful", {
+    position: "bottom-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+const notifyError = (message: string) => {
+  toast.error(message, {
+    position: "bottom-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+const notifyGeneralError = () => {
+  toast.error("An error occurred. Please try again.", {
+    position: "bottom-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+const notifyAuthenticationError = () => {
+  toast.error("Authentication error. Please log in again.", {
+    position: "bottom-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
 
 class AxiosWithInterceptor {
   instance: any;
@@ -25,13 +73,25 @@ class AxiosWithInterceptor {
 
     this.instance.interceptors.response.use(
       (response: any) => {
-        setTimeout(() => setIsLoading(false), 1000); 
-        //notifySuccess();
+        setTimeout(() => setIsLoading(false), 1000);
+        notifySuccess();
         return response;
       },
       (error: any) => {
-        setTimeout(() => setIsLoading(false), 100); 
-        //notifyError();
+        setTimeout(() => setIsLoading(false), 100);
+
+        if (!error.response) {
+          notifyGeneralError();
+        } else if (!error.response.data) {
+          if (error.response.status >= 400 && error.response.status < 500) {
+            notifyAuthenticationError();
+          } else {
+            notifyGeneralError();
+          }
+        } else {
+          notifyError(error.response.data.message || "An error occurred");
+        }
+
         return Promise.reject(error);
       }
     );
