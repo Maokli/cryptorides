bfd2c28eda2531f70eb9a9e32a04e21fedc465db
Maokli <louayinfo07@gmail.com>
2024-05-27 17:34:33 +0100

    use decorators

--- api/src/auth/jwt-auth.guard.ts
+++ api/src/auth/jwt-auth.guard.ts
@@ -1,15 +1,36 @@
 /* eslint-disable prettier/prettier */
 
-import { Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { GqlExecutionContext } from "@nestjs/graphql";
+import * as jwt from 'jsonwebtoken';
+
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
   constructor() {
     super();
   }
-  getRequest(context: GqlExecutionContext) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
+    const request = ctx.getContext().req;
+    
+    const authHeader = request.headers['authorization'] as string;
+    if (!authHeader) {
+      throw new UnauthorizedException();
+    }
+
+    const token = authHeader.split(' ')[1];
+  
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const decodedToken = jwt.verify(token,process.env.JWT_SECRET)as { userId: number };
+  
+      request['user'] = decodedToken;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
   }
 }
